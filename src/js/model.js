4f975ffb083ff7c4e6b87d163505dcdb19212725
Abdelrahman Emara <abdelrahmanemara155@gmail.com>
2025-09-02 23:38:32 +0300

    Create Helpers and Configuration Files

--- src/js/model.js
+++ src/js/model.js
@@ -1,19 +1,14 @@
 import 'regenerator-runtime/runtime';
+import { API_URL } from './config';
+import { getJSON } from './helper';
 
 export const state = {
   recipe: {},
 };
 
-export const loadRecipe = async function(id) {
+export const loadRecipe = async function (id) {
   try {
-    const res = await fetch(
-      `https://forkify-api.jonas.io/api/v2/recipes/${id}`
-    );
-
-    const data = await res.json();
-
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-    // console.log(res, data);
+    const data = await getJSON(`${API_URL}/${id}`);
 
     const { recipe } = data.data;
     state.recipe = {
@@ -27,6 +22,6 @@ export const loadRecipe = async function(id) {
       servings: recipe.servings,
     };
   } catch (err) {
-    alert(err);
+    console.error(`${err} 🔥🔥🔥`);
   }
 };
