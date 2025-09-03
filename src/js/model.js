6f603673c979d3f087788b590218927c5c48305c
Abdelrahman Emara <abdelrahmanemara155@gmail.com>
2025-09-03 14:20:03 +0300

    Implementing Error and Success Messages

--- src/js/model.js
+++ src/js/model.js
@@ -22,6 +22,7 @@ export const loadRecipe = async function (id) {
       servings: recipe.servings,
     };
   } catch (err) {
-    console.error(`${err} 🔥🔥🔥`);
+    console.error(`${err} 🔥🔥`);
+    throw err;
   }
 };
--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -6,6 +6,8 @@ import icons from 'url:../../img/icons.svg'; // parcel 2
 class RecipeView {
   #parentElement = document.querySelector('.recipe');
   #data;
+  #errorMessage = 'We could not find that recipe. Please try another one!';
+  #message = '';
 
   render(data) {
     this.#data = data;
@@ -27,9 +29,41 @@ class RecipeView {
       </div>
     `;
 
-    this.#parentElement.innerHTML = '';
+    this.#clear();
     this.#parentElement.insertAdjacentHTML('afterbegin', markup);
   };
+
+  renderMessage(message = this.#message) {
+    const markup = `
+      <div class="message">
+        <div>
+          <svg>
+            <use href="${icons}#icon-smile"></use>
+          </svg>
+        </div>
+        <p>${message}</p>
+      </div>
+    `;
+
+    this.#clear();
+    this.#parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  renderError(message = this.#errorMessage) {
+    const markup = `
+      <div class="error">
+        <div>
+          <svg>
+            <use href="${icons}#icon-alert-triangle"></use>
+          </svg>
+        </div>
+        <p>${message}</p>
+      </div>
+    `;
+
+    this.#clear();
+    this.#parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
 
   addHandlerRender(handler) {
     ['hashchange', 'load'].forEach(ev => window.addEventListener(ev, handler));
