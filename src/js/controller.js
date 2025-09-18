9f5b70bfbdbd3bfd311568843886e47dd65ae064
Abdelrahman Emara <abdelrahmanemara155@gmail.com>
2025-09-18 14:58:29 +0300

    Implement showing bookmarks in list and create Preview file

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,6 +3,7 @@ import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
 import paginationView from './views/paginationView.js';
+import bookmarksView from './views/bookmarksView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -20,6 +21,7 @@ const controlRecipes = async function () {
 
     // 0) Update results view to mark selected search result
     resultsView.update(model.getSearchResultsPage());
+    bookmarksView.update(model.state.bookmarks);
 
     // 1) Loading a Recipe
     await model.loadRecipe(id);
@@ -70,13 +72,16 @@ const controlUpdateServings = function (newServings) {
 };
 
 const controlAddBookmark = function () {
-  // Add/remove bookmark
+  // 1) Add/remove bookmark
   if (model.state.recipe.bookmarked)
     model.deleteBookmark(model.state.recipe.id);
   else model.addBookmark(model.state.recipe);
 
-  // update recipe view
+  // 2) Update recipe view
   recipeView.update(model.state.recipe);
+
+  // 3) Render bookmarks
+  bookmarksView.render(model.state.bookmarks);
 };
 
 const init = function () {
---
+++ src/js/views/bookmarksView.js
@@ -0,0 +1,16 @@
+import icons from 'url:../../img/icons.svg'; // parcel 2
+import View from './View.js';
+import PreviewView from './PreviewView.js';
+class BookmarksView extends View {
+  _parentElement = document.querySelector('.bookmarks__list');
+  _errorMessage = '';
+  _message = 'No bookmarks yet. Find a nice recipe and bookmark it.';
+
+  _generateMarkup() {
+    return this._data
+      .map(bookMark => PreviewView.render(bookMark, false))
+      .join('');
+  }
+}
+
+export default new BookmarksView();
